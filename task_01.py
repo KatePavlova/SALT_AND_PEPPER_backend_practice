84b35d64bb5c53333a4a328e313e405a4b671626
TriwOnline <55847564+KatePavlova@users.noreply.github.com>
2025-05-25 18:39:05 +0300

    Update task_01.py

--- task_01.py
+++ task_01.py
@@ -5,7 +5,7 @@ def is_palindrome(string):
     new_string = re.sub(r'([\s\W_])', '', string.lower())
     return new_string == new_string[::-1]
 
-
+'''
 print(is_palindrome("A man, a plan, a canal -- Panama"))    # => True
 print(is_palindrome("Madam, I'm Adam!"))                    # => True
 print(is_palindrome(333))                                   # => True
@@ -13,5 +13,5 @@ print(is_palindrome(None))                                  # => False
 print(is_palindrome("Abracadabra"))                         # => False
 print(is_palindrome([1, 32.3, 1]))                          # => True
 print(is_palindrome(" "))                                   # => True
-
+'''
 
